f9c88cf984cbc39caf34191bde3e4cd4491869a3
h21565897 <h21565897@gmail.com>
2023-09-02 21:10:01 -0700

    optimize layout

--- app/(userauth)/layout.tsx
+++ app/(userauth)/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="">
-        <div className="flex items-center justify-center w-screen h-screen">
+        <div className="flex items-center justify-center w-screen">
           {children}
         </div>
       </body>
